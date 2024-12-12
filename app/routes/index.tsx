49a239d2497980693e50e8d38a73b53bcb16facc
JamesUlph <james.ulph@hotmail.co.uk>
2024-12-12 19:52:30 +0000

    Init

---
+++ app/routes/index.tsx
@@ -0,0 +1,27 @@
+import { createFileRoute, useRouter } from '@tanstack/react-router';
+import { getCount, updateCount } from '../functions';
+
+export const Route = createFileRoute('/')({
+  component: Home,
+  loader: async () => await getCount(),
+});
+
+function Home() {
+  const state = Route.useLoaderData();
+  const router = useRouter();
+
+  return (
+    <button
+      type="button"
+      onClick={() => {
+        updateCount({
+          data: {
+            increment: 2,
+          },
+        }).then(() => router.invalidate());
+      }}
+    >
+      Add to {state} {state}
+    </button>
+  );
+}
